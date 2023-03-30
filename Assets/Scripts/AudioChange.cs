eba42436e58c4adb89ae7f3a76df22cfb21eec44
RemiRaff <remi_raffaghello@hotmail.com>
2023-03-30 11:13:17 +0200

    Music added for each scene

--- Assets/Scripts/AudioChange.cs
+++ Assets/Scripts/AudioChange.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class AudioChange : MonoBehaviour
 {
-    [SerializeField] AudioClip[] audioClips;
-    [SerializeField] int musicID = 0;
+    [SerializeField] AudioClip [] audioClips;
 
     private AudioSource myAudioSource;
+    private int musicID = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,20 @@ public class AudioChange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*myAudioSource.clip = audioClips[musicID];
-        myAudioSource.Play();*/
+        // change de music avec les scenes
+        SetNewClip(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void SetNewClip(int clipID)
+    {
+        if ((0 < clipID) && (clipID < audioClips.Length))
+        {
+            if (clipID != musicID)
+            {
+                musicID = clipID;
+                myAudioSource.clip = audioClips[clipID];
+                myAudioSource.Play();
+            }
+        }
     }
 }
